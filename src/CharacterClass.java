fde772964d43f0bfce1a6c060a4b37c55ef039a7
James Cassells <w0517106@nscc.ca>
2026-03-09 22:16:58 -0300

    CharacterClass created

---
+++ src/CharacterClass.java
@@ -0,0 +1,32 @@
+public abstract class CharacterClass {
+    private String className;
+    private String armourType;
+
+    public CharacterClass(String className, String armourType) {
+        this.className = className;
+        this.armourType = armourType;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getArmourType() {
+        return armourType;
+    }
+
+    public void setArmourType(String armourType) {
+        this.armourType = armourType;
+    }
+
+    public abstract int getClassBonus();
+
+    @Override
+    public String toString() {
+        return className + " (Armour Type: " + armourType + ")";
+    }
+}
