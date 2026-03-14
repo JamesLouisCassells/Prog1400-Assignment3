01f42c7eadd43f21013505b56f8ec631dac3a561
James Cassells <w0517106@nscc.ca>
2026-03-14 15:58:16 -0300

    Beginning the large, crucial class: CreationPanel

---
+++ src/GUI/CreationPanel.java
@@ -0,0 +1,18 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class CreationPanel extends JPanel {
+
+    private GameFrame frame;
+
+    public CreationPanel(GameFrame frame) {
+
+        this.frame = frame;
+
+        setBackground(new Color(140,155,220));
+        setLayout(null);
+
+    }
+}
