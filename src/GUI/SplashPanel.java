d2af4232fb1bd52b9d135609c5d4f9d050d947ec
James Cassells <w0517106@nscc.ca>
2026-03-14 15:26:52 -0300

    Added a custom background image for the splashpanel and linked it to my images folder

--- src/GUI/SplashPanel.java
+++ src/GUI/SplashPanel.java
@@ -1,3 +1,8 @@
+/*
+a title
+an image placeholder
+a button
+ */
 package GUI;
 import javax.swing.*;
 import java.awt.*;
@@ -9,24 +14,24 @@ public class SplashPanel extends JPanel { //inherits from JPanel
     public SplashPanel(GameFrame p_frame) { //constructor receives the GameFrame
         this.frame = p_frame; //saves the passed-in GameFrame
 
-        setBackground(new Color(140, 155, 220)); //sets the panel background colour
+        setBackground(new Color(138, 154, 91)); //sets the panel background colour to moss green
         setLayout(null); //uses manual positioning for components
 
+        //Title
         JLabel titleLabel = new JLabel("Dragons and Dungeons!!"); //creates the title text
         titleLabel.setFont(new Font("Calibri", Font.BOLD, 28)); //this sets title font stylisation
         titleLabel.setBounds(240, 50, 350, 40); //sets title position and sizing
         add(titleLabel); //adds title to panel!
-
-        JLabel imageLabel = new JLabel("Splash Image Here", SwingConstants.CENTER); //placeholder image label
-        imageLabel.setOpaque(true); //makes background colour visible
-        imageLabel.setBackground(Color.WHITE); //sets placeholder background colour
+        //Image
+        ImageIcon splashImage = new ImageIcon("images/creation.png");
+        JLabel imageLabel = new JLabel(splashImage); //placeholder image label
         imageLabel.setBounds(120, 120, 550, 280); //sets image area position and size
         add(imageLabel); //adds image placeholder to panel
-
-        JButton buildButton = new JButton("Build A Character"); //creates button to continue
+        //Button
+        JButton buildButton = new JButton("Build A Character"); //creates button to begin making a character
         buildButton.setFont(new Font("Calibri", Font.BOLD, 22)); //sets button font
         buildButton.setBounds(250, 460, 250, 40); //sets button position and size
         buildButton.addActionListener(p_event -> frame.showCreationPanel()); //switches to creation screen when clicked
-        add(buildButton); //adds button to panel
+        add(buildButton); //adds button to panel (very important)
     }
 }
